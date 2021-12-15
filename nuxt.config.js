d1113268b18eb1d0e560fd6f8ebc7e2bdfb8edae
Pranav Raut <pranavraut033@gmail.com>
2021-12-15 13:58:15 +0530

    chore: added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,20 +14,61 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Unbound' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'msapplication-TileColor',
+        content: '#ffffff',
+      },
+      {
+        name: 'msapplication-config',
+        content: '/icons/browserconfig.xml',
+      },
+      {
+        name: 'theme-color',
+        content: '#ffffff',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/icons/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/icons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/icons/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/icons/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+      {
+        rel: 'shortcut icon',
+        href: '/icons/favicon.ico',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    './plugins/v-tooltip.js',
-    './plugins/v-clipboard.js'
-  ],
+  plugins: ['./plugins/v-tooltip.js', './plugins/v-clipboard.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
